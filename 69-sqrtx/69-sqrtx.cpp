37e86f412452d9e705f34f54cdbc331aab1faacb
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-03-25 09:49:08 +0530

    Time: 4 ms (57.60%), Space: 5.8 MB (95.60%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -4,15 +4,15 @@ public:
         long int l=0,h=x;
         while(l<=h)
         {
-            long int mid=l+(h-l)/2;
-            if(mid*mid==x || (mid*mid<x && (mid+1)*(mid+1)>x))
+            long int mid=l+(h-l)/2; 
+            if(mid*mid==x || (mid*mid<x && (mid+1)*(mid+1)>x)) 
             {
              return mid;
             }
             else if(mid*mid<x)
                 l=mid+1;
-            else
-                h=mid-1;
+            else 
+                h=mid-1; 
         }
         return 0;
     }
