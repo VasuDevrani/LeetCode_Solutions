12ac7b36f26e107e7614ec0b5d35f1b53f7e0971
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-04-14 11:15:31 +0530

    Time: 55 ms (21.74%), Space: 45.8 MB (10.28%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-    bool isSafe1(int row, int col, vector<string> board, int n) {
-        // check upper diagonal
-        int duprow = row;
-        int dupcol = col; 
+    bool isSafe1(int i, int col, vector<string> board, int row) {
         
-        while(row >= 0 && col >= 0) {
-            if(board[row][col] == 'Q')
+        for(int j=col;j>=0;j--)
+        {
+            if(board[i][j]=='Q')
                 return false;
-            row--;
+        }
+        int col1=col;
+        for(int j=i;j>=0 && col>=0;j--)
+        {
+            if(board[j][col]=='Q')
+                return false;
             col--;
         }
-        col = dupcol; 
-        row = duprow;
-        while(col>=0) {
-            if(board[row][col] == 'Q') 
+        for(int j=i;j<row && col1>=0;j++)
+        {
+            if(board[j][col1]=='Q')
                 return false;
-            col--; 
+            col1--;
         }
-    
-        row = duprow;
-        col = dupcol; 
-        while(row<n && col>=0) {
-            if(board[row][col] == 'Q') 
-                return false;
-            row++;
-            col--; 
-        }
-        
-        return true; 
+
+            return true;
     }
 
     void get_boards(vector<string>&board,vector<vector<string>>&ans,int col,int row)
@@ -63,30 +56,3 @@ public:
             return ans;
     }
 };
-    
-    
-    
-//     bool place_queen(vector<string>&board,int col,int i,int row)
-//     {
-//         for(int j=col;j>=0;j--)
-//         {
-//             if(board[i][j]=='Q')
-//                 return false;
-//         }
-//         int col1=col;
-//         for(int j=i;j>=0 && col>=0;j--)
-//         {
-//             if(board[j][col]=='Q')
-//                 return false;
-//             col--;
-//         }
-//         for(int j=i;j<row && col1<row;j++)
-//         {
-//             if(board[j][col1]=='Q')
-//                 return false;
-//             col1--;
-//         }
-
-//             return true;
-//     }
-    
