e17a391ebf224151914f43141639f9fa6aa5c17a
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-05-01 19:57:41 +0530

    Time: 192 ms (70.67%), Space: 85 MB (95.93%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& t) {
-        vector<int>ngreat;
+        int n=t.size();
+        
+        vector<int>ans(n,0);
         stack<int>temp;
-        ngreat.push_back(-1);
-        temp.push(t.size()-1);
-        vector<int>ans;
         
-        
-        for(int i=t.size()-2;i>=0;i--)
+        for(int i=n-1;i>=0;i--)
         {
-            while(!temp.empty())
-            {
-                if(t[temp.top()]>t[i])
-                {
-                    ngreat.push_back(temp.top());
-                    temp.push(i);
-                    break;
-                }
-                else
-                    temp.pop();
-            }
-            if(temp.empty())
-            {
-                temp.push(i);
-                ngreat.push_back(-1);
-            }
-        }
-            reverse(ngreat.begin(),ngreat.end());
+        while(!temp.empty() && t[i] >= t[temp.top()])
+            temp.pop();
+        if(temp.empty())
+            ans[i] = 0;
+        else
+            ans[i] = temp.top() - i;
         
-        for(int i=0;i<ngreat.size();i++)
-        {
-            if(ngreat[i]==-1)
-                ans.push_back(0);
-            else
-                ans.push_back(ngreat[i]-i);
+        temp.push(i);
         }
         return ans;
+      
     }
 };
