08fd34ae32670df9e56db456c3388a369ee026d6
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-05-06 14:40:38 +0530

    Time: 205 ms (43.69%), Space: 57.3 MB (66.77%) - LeetHub

--- 933-number-of-recent-calls/933-number-of-recent-calls.cpp
+++ 933-number-of-recent-calls/933-number-of-recent-calls.cpp
@@ -1,8 +1,6 @@
 class RecentCounter {
 public:
     queue<int>pings;
-    vector<int>pin;
-    int j=0;
     
     RecentCounter() {
         
@@ -11,22 +9,11 @@ public:
     int ping(int t) {
         int x=t-3000;
         pings.push(t);
-        pin.push_back(t);
         
-        int ct=0;
-        for(int i=pin.size()-1;i>=j;i--)
-        {
-            if(pin[i]>=x && pin[i]<=t)
-            {
-                ct++;
-            }
-            else
-            {
-                pings.pop();
-                j++;
-            }
-        }
-        return ct;
+        while(pings.front()<x)
+            pings.pop();
+        
+        return pings.size();
     }
 };
 
