a1514c7a299780fef9633f38f567dd0fa899a6a9
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-05-28 09:27:15 +0530

    Time: 38 ms (20.53%), Space: 17.9 MB (65.37%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]!=i)
+        sort(nums.begin(),nums.end());
+        int ans;
+        
+        for(int i=0;i<nums.size()-1;i++)
             {
-             return i;
+                if(nums[i+1]-nums[i]!=1)
+                {
+                    ans=nums[i]+1;
+                    break;
+                }
             }
-        }
-        return nums.size();
+        if(nums[0]!=0)
+            ans=0;
+        if(nums[nums.size()-1]!=nums.size())
+            ans=nums.size();
+        return ans;
     }
 };
