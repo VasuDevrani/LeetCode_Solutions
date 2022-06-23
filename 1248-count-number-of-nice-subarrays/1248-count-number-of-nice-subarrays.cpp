9b7187979bb74a3eb7a89f2f7abf941548edc1ed
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-06-23 11:47:21 +0530

    Time: 379 ms (19.16%), Space: 90.4 MB (20.16%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int ans = 0, sum = 0;
+        unordered_map<int,int> freq;
+        
+        for(auto a:nums)
+        {
+            if(a%2 != 0) 
+                sum++;
+            if(sum == k) 
+                ans++;
+            if(freq.count(sum-k)) 
+                ans += freq[sum-k];
+            freq[sum]++;
+        }
+        return ans;
+    }
+};
