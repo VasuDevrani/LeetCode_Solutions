f1ae5c3ca3442783a8b82bd38363f67edfacbada
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-06-23 11:36:42 +0530

    Time: 215 ms (5.10%), Space: 47.2 MB (8.83%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -3,16 +3,19 @@ public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         vector<int>ans;
         
+//         creating a difference array to store the absolute diff
         int diff[arr.size()];
         for(int i=0;i<arr.size();i++)
             diff[i] = abs(x - arr[i]);
         
+//         the map will contain order of occurence of differences in diff array
         map<int, vector<int>>m;
         for(int i=0;i<arr.size();i++)
         {
             m[diff[i]].push_back(i);
         }
         
+//         finally traversing the map and getting the closest element form the index stored
         for(auto x:m)
         {
             for(int i=0;i<x.second.size();i++)
@@ -26,23 +29,9 @@ public:
                 break;
         }
         
+//         sorting in increasing order
         sort(ans.begin(), ans.end());
         
         return ans;
-        
-        
-        
-//         if(arr[0] >= x)
-//         {
-//             for(int i=0;i<k;i++)
-//                 ans.push_back(arr[i]);
-//             return ans;
-//         }
-//         else
-//         {
-//             int ind = upper_bound(arr.begin(), arr.end(), x) - arr.begin();
-            
-//             int 
-//         }
     }
 };
