e58ba51a2ffe5f9d9d5c65f0097d161694140389
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-06-23 09:56:04 +0530

    Time: 58 ms (5.36%), Space: 19.7 MB (13.25%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -4,12 +4,10 @@ public:
         unordered_map<int,int>m;
         
         for(int i=0;i<nums.size();i++)
-            m[nums[i]]++;
-        
-        for(auto x:m)
         {
-            if(x.second > nums.size()/2)
-                return x.first;
+            m[nums[i]]++;
+            if(m[nums[i]] > nums.size()/2)
+                return nums[i];
         }
         return -1;
     }
