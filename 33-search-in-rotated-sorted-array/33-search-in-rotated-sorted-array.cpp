56f8efa1f04aa4f149be63eeb6448d133f3e205c
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-06-23 21:28:43 +0530

    Time: 12 ms (11.77%), Space: 11 MB (74.82%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    
     int search(vector<int>& nums, int target) {
         int n=nums.size();
         int start=0, end=n-1;
@@ -22,40 +21,4 @@ public:
         }
         return -1;
     }
-//     int pivot_find(vector<int>v)
-//     {
-//         int l=0,h=v.size()-1;
-//         while(l<h)
-//         {
-//             int mid=l+(h-l)/2;
-//             if(v[mid]>=v[0])
-//                 l=mid+1;
-//             else 
-//                 h=mid;
-//         }
-//         return l;
-//     }
-    
-//     int search(vector<int>& nums, int target) {
-//         int p=pivot_find(nums);
-        
-//         if(target==nums[p])
-//             return p;
-//         else if(target>nums[p] && target<nums[0])
-//         {
-//             int i= lower_bound(nums.begin()+p,nums.end(),target)-nums.begin();
-//             if(nums[i]==target)
-//                 return i;
-//             else
-//                 return -1;
-//         }
-//         else
-//         {
-//             int i= lower_bound(nums.begin(),nums.begin()+p-1,target)-nums.begin();
-//             if(nums[i]==target)
-//                 return i;
-//             else
-//                 return -1;
-//         }
-    
 };
