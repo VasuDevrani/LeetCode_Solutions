b3905df6d406a34498a9b15143b9ba8f94e36725
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-06-27 16:49:03 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.54%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    
-    int getCount(int n, long int dp[])
-    {
-//         base case
-        if(n == 0)
-            return 1;
-//         recursive case
-        
-        if(dp[n] != 0)
-            return dp[n];
-        
-        long int ways = 0;
-        for(int i=1;i<=2;i++)
-        {
-            if(n-i >= 0)
-            {
-                ways += getCount(n-i, dp);
-            }
-        }
-        return dp[n] = ways;
-    }
-    
     int climbStairs(int n) {
         long int dp[n+1];
-        
         for(int i=0;i<n+1;i++)
             dp[i] = 0;
         
-        return getCount(n, dp);
+        dp[0] = 1;
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<=2;j++)
+            {
+                if(i - j >= 0)
+                {
+                    dp[i] += dp[i-j];
+                }
+            }
+        }
+        return dp[n];
     }
 };
