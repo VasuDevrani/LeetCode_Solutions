840bcf65462664ada551b320289b72ec042761fa
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-07-01 14:46:02 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (46.93%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,45 +1,26 @@
 class Solution {
 public:
-    
-    int getAns(vector<int>nums, int i, int dp[])
-    {
-//         base case
-        if(i >= nums.size())
-            return 0;
-        
-//         recursive case
-        if(dp[i] != -1)
-            return dp[i];
-        
-        int mx = 0;
-        int j = i+2;
-        
-        while(1)
-        {
-            int sum = nums[i] + getAns(nums, j, dp);
-            cout<<sum<<" ";
-            mx = max(sum, mx);
-            if(j >= nums.size())
-                break;
-            j++;
-        }
-        
-        return dp[i] = mx;
-    }
-    
     int rob(vector<int>& nums) {
-        
-        if(nums.size() == 1)
+        int n = nums.size();
+        if(n == 0)
+            return 0;
+        if(n == 1)
             return nums[0];
         
-        int mx = 0;
-        int dp[nums.size()];
+        vector<int>dp(n+1, 0);
+        dp[0] = 0; //for i<0
+        dp[1] = nums[0]; //for i=0
+            
+        for(int i=1;i<n;i++) //indexes shows the index of nums
+        {
+//             include case
+            int incl = dp[i-1] + nums[i]; //i-1 of nums is i-2 of dp
+//             exclude case
+            int excl = dp[i];
+            
+            dp[i+1] = max(incl, excl);
+        }
         
-        for(int i=0;i<nums.size();i++)
-            dp[i] = -1;
-    
-        mx = max(getAns(nums, 0, dp), getAns(nums, 1, dp));
-        
-        return mx;
+        return dp[n];
     }
 };
