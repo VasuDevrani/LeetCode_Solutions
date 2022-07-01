9fbc6bbacd2ed36bb7c49d3290c010903e261165
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-07-01 19:15:29 +0530

    Time: 421 ms (28.52%), Space: 113.4 MB (63.47%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -11,42 +11,45 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        vector<int>cr, ans;
+        vector<int>ans;
+        
+        int first_ind, prev_ind = 0, last_ind, ct = 0;
         
         int prev = head->val;
         head = head->next;
-        int i = 2;
+        int i = 2, mn = INT_MAX;
         
         while(head->next != NULL)
         {
-            if(head->next->val < head->val && head->val > prev)
-                cr.push_back(i);
-            if(head->next->val > head->val && head->val < prev)
-                cr.push_back(i);
+            if((head->next->val < head->val && head->val > prev) || (head->next->val > head->val && head->val < prev))
+            {
+                if(ct == 0)
+                    first_ind = i;
+                
+                last_ind = i;
+                if(ct > 0)
+                    mn = min(mn, last_ind - prev_ind);
+                
+                prev_ind = last_ind;
+                    
+                ct++;
+            }
             
             prev = head->val;
             head = head->next;
             i++;
         }
-        
-        if(cr.size() < 2)
+        if(ct < 2)
         {
             ans.push_back(-1);
             ans.push_back(-1);
-            
             return ans;
         }
-        
-        sort(cr.begin(), cr.end());
-        
-        int mn = INT_MAX;
-        for(int i=0;i<cr.size()-1;i++)
-        {
-            mn = min(mn, cr[i+1] - cr[i]);
-        }
+
+        cout<<first_ind<<" "<<last_ind<<" "<<mn;
         
         ans.push_back(mn);
-        ans.push_back(cr[cr.size()-1] - cr[0]);
+        ans.push_back(last_ind - first_ind);
         
         return ans;
     }
