d3746fbbdc35947738c7ba841ce09cda400fa1a3
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-07-02 11:02:09 +0530

    Time: 33 ms (27.72%), Space: 16.4 MB (16.57%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,51 +1,22 @@
 class Solution {
 public:
-    
-    int getAns(vector<pair<int,int>>v, int i, vector<int>&dp)
-    {
-//         base case
-        if(i < 0)
-            return 0;
-        if(i == 0)
-            return (v[0].first * v[0].second);
-        
-        if(dp[i] != -1)
-            return dp[i];
-        
-//         recursive case
-        if(v[i-1].first == v[i].first - 1){
-            int incl = getAns(v, i-2, dp) + (v[i].second * v[i].first);
-            int excl = getAns(v, i-1, dp);
-
-            return dp[i] = max(incl, excl);
-        }
-        else{
-            return dp[i] = getAns(v, i-1, dp) + (v[i].second * v[i].first);
-        }
-    }
-    
     int deleteAndEarn(vector<int>& nums) {
+        int n = 10001;
         
-        if(nums.size() == 1)
-            return nums[0];
+        vector<int>sum(n, 0);
+        vector<int>dp(n, 0);
         
-        unordered_map<int,int>m;
+        for(auto x:nums)
+            sum[x] += x;
         
-        int n = nums.size();
-        for(int i=0;i<n;i++)
-            m[nums[i]]++;
+        dp[0] = 0;
+        dp[1] = sum[1];
         
-        vector<pair<int,int>>arr;
+        for(int i=2;i<n;i++)
+        {
+            dp[i] = max(dp[i-2] + sum[i], dp[i-1]); 
+        }
         
-        for(auto x:m)
-            arr.push_back(make_pair(x.first, x.second));
-        
-        sort(arr.begin(), arr.end());
-        
-        vector<int>dp(arr.size(), -1);
-        
-        getAns(arr, arr.size()-1, dp);
-        
-        return dp[arr.size()-1];
+        return dp[n-1];
     }
 };
