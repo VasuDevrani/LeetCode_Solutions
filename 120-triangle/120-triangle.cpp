d0343556c8eb1c096ee31c2040c9973c3629d694
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-07-04 23:55:55 +0530

    Time: 11 ms (44.14%), Space: 9.3 MB (7.84%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,20 +1,46 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& tri) {
+    
+//     int getMinRecursive(vector<vector<int>>triangle, int j, int i, int n)
+//     {
+// //         base case
+//         if(i > n)
+//             return 0;
+//         if(i == n)
+//            return min(triangle[i][j], triangle[i][j+1]);
         
-//         bottom up approach to get the min_sum path
+// //         recursive case
+//         int left = triangle[i][j] + getMinRecursive(triangle, j, i+1, n);
+//         int right = triangle[i][j+1] + getMinRecurisve(triangle, j+1, i+1, n);
         
-//         first loop traverses from second last row 
-        for(int i=tri.size()-2;i>=0;i--)
+//         return min(right, left);
+//     }
+    
+    int BotUp(vector<vector<int>>triangle, int n)
+    {
+        vector<vector<int>>dp(n, vector<int>(n, 0));
+        
+        for(int i=0;i<n;i++)
+            dp[n-1][i] = triangle[n-1][i];
+        
+        for(int i=n-2;i>=0;i--)
         {
-//             this loops keeps storing the min values from lower rows
-            for(int j=0;j<tri[i].size();j++)
+            for(int j=i;j>=0;j--)
             {
-                tri[i][j] = tri[i][j] + min(tri[i+1][j+1], tri[i+1][j]);
+                int right = triangle[i][j] + dp[i+1][j];
+                int left = triangle[i][j] + dp[i+1][j+1];
+                
+                dp[i][j] = min(right, left);
             }
         }
-            
-//         finally the lowest sum values get collected at top and we return top
-        return tri[0][0];
+        return dp[0][0];
+    }
+    
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        
+        // return triangle[0][0] + getMinRecursive(triangle, 0, 1, n-1);
+        
+        return BotUp(triangle, n);
     }
 };
