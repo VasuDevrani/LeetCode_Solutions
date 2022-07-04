7d16310dd0939ec0e8a00e1dfa1d27ab712610a5
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-07-04 22:25:48 +0530

    Time: 8 ms (26.35%), Space: 8 MB (12.42%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -39,6 +39,33 @@ public:
         return dp[r][c] = top + left;
     }
     
+    int BotUp(vector<vector<int>>grid, int m, int n){
+        vector<vector<int>>dp(m, vector<int>(n, 0));
+        
+        if(grid[0][0] == 1)
+            return 0;
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++)
+            {
+                if(i == 0 && j == 0)
+                    dp[0][0] = 1;
+                else if(grid[i][j] == 1)
+                    dp[i][j] = 0;
+                else{
+                    int down = 0, right = 0;
+                    if(i > 0)
+                        down = dp[i-1][j];
+                    if(j > 0)
+                        right = dp[i][j-1];
+                    
+                    dp[i][j] = right + down;    
+                }
+            }   
+        }
+        return dp[m-1][n-1];
+    }
+    
     int uniquePathsWithObstacles(vector<vector<int>>& og) {
         int m = og.size();
         int n = og[0].size();
@@ -46,6 +73,8 @@ public:
         // return recursive(m-1, n-1, og);
         
         vector<vector<int>>dp(m, vector<int>(n, -1));
-        return TopBot(m-1, n-1, og, dp);
+        // return TopBot(m-1, n-1, og, dp);
+        
+        return BotUp(og, m, n);
     }
 };
