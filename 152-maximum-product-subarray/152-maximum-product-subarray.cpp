5412c2923a1f8ac98a54567710398d0227575848
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-07-06 18:02:17 +0530

    Time: 13 ms (40.68%), Space: 13.8 MB (71.77%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+//     dry run here: -2,2,4,3,0,1,-2,-1
+    
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
         int ans = nums[0];
@@ -7,9 +9,11 @@ public:
         int mn = ans, mx = ans;
         for(int i=1;i<n;i++)
         {
+//             whenever there's a negative element, it is gonna reduce the max and increase the small, so swap them...
             if(nums[i] < 0)
                 swap(mn, mx);
             
+//             this operation ensures the regeneration of product if 0 has arrived
             mx = max(nums[i], mx*nums[i]);
             mn = min(nums[i], mn*nums[i]);
             
