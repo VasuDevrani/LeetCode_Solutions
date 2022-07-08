ac34cd95d0ffa39544a0d0b79c93a6daf2c2672a
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-07-08 08:17:17 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (13.67%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -31,10 +31,36 @@ public:
         return dp[i][ct] = take + not_take;
     }
     
+    int BotTop(int ct, string vowel, int n)
+    {
+        vector<vector<int>>dp(n, vector<int>(ct+1, 0));
+        
+        for(int i=0;i<n;i++)
+            dp[i][0] = 1;
+        
+        for(int j=0;j<=ct;j++)
+            dp[4][j] = 1;
+        
+        for(int i=3;i>=0;i--) //index
+        {
+            for(int j=1;j<=ct;j++) //count
+            {
+                int not_take = dp[i+1][j];
+                int take = dp[i][j-1];
+
+                dp[i][j] = take + not_take; 
+            }
+        }
+        
+        return dp[0][ct];
+    }
+    
     int countVowelStrings(int n) {
         string vowel = "aeiou";
         
-        vector<vector<int>>dp(5, vector<int>(n+1, -1));
-        return TopBot(0, n, vowel, dp);
+        // vector<vector<int>>dp(5, vector<int>(n+1, -1));
+        // return TopBot(0, n, vowel, dp);
+        
+        return BotTop(n, vowel, 5);
     }
 };
