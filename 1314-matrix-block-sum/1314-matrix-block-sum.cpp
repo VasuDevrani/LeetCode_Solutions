4d12ea31ef1028d6144e4b00d054f27c14671721
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-07-14 00:32:23 +0530

    Time: 12 ms (76.32%), Space: 9.5 MB (51.47%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -8,6 +8,7 @@ public:
         //Bottom-up dp approach
         vector<vector<int> > dp(m+1,vector<int>(n+1,0));
         
+//         calculating the prefix sum
         for (int i = 1; i <= m; i++) 
         {
             for (int j = 1; j <= n; j++) 
@@ -29,6 +30,13 @@ public:
                 
                 //value of the block
                 ans[i][j]=dp[r2][c2]+dp[r1-1][c1-1]-dp[r1-1][c2]-dp[r2][c1-1]; 
+                
+                //explanation for above formula
+                
+                // +(r2, c2) is like adding the sum of all elements of upto the last edge of req block
+                // -(r2, c1-1) is like removing the sum of elements before first horizontal edge
+                // -(r1-1, c2) is like removing the sum of elements before first vertical edge
+                // +(r1-1, c1-1) is for adding the part which has been removed twice during last two operations
             }
         }
       return ans;  
