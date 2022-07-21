6b967c059f17407ae6cf0fecf9725b26641b39c4
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-07-21 08:31:18 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (98.50%) - LeetHub

---
+++ 1405-longest-happy-string/1405-longest-happy-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c) {
+        string ans="";
+        int size = a + b + c;
+        
+        int A = 0, B = 0, C = 0;
+        
+        for(int i = 0; i < size; i++) 
+        {
+            if ((a >= b && a >= c && A != 2) || (B == 2 && a > 0) || (C == 2 && a > 0))
+            {
+                ans += "a";
+                a--,A++,B = 0,C = 0;  
+            }
+            else if ((b >= a && b >= c && B != 2) || (A == 2 && b > 0) || (C == 2 && b > 0))
+            {
+                ans += "b";
+                b--,B++,A = 0,C = 0;
+            }
+            else if ((c >= a && c >= b && C != 2) || (B == 2 && c > 0) || (A == 2 && c > 0))
+            {
+                ans += "c";
+                c--,C++,A = 0,B = 0;  
+            }
+        }
+        return ans;
+    }
+};
