2aeb54e44f7a56d010ce6e8a16db529ffc28f8b8
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-07-29 19:15:45 +0530

    Time: 8 ms (83.57%), Space: 8.2 MB (51.73%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.cpp
+++ 419-battleships-in-a-board/419-battleships-in-a-board.cpp
@@ -1,36 +1,12 @@
 class Solution {
 public:
-    
-    void markAll(int i, int j, int n, int m, vector<vector<char>>& board)
-    {
-        if(i >= 0 && j >= 0 && j < m && i < n && board[i][j] == 'X')
-        {
-            board[i][j] ='.';
-            markAll(i+1, j, n, m, board);
-            markAll(i, j+1, n, m, board);
-            markAll(i, j-1, n, m, board);
-            markAll(i-1, j, n, m, board);
-        }
-        return;
-    }
-    
     int countBattleships(vector<vector<char>>& board) {
-        int ans = 0;
+        int count = 0;
         
-        int n = board.size(), m = board[0].size();
+    for(int i=0;i<board.size();i++)
+        for(int j=0;j<board[0].size();j++)
+            if(board[i][j]=='X' && (i==0 || board[i-1][j]!='X') && (j==0 || board[i][j-1]!='X')) count++;
         
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(board[i][j] == 'X')
-                {
-                    ans++;
-                    markAll(i, j, n, m, board);
-                }
-            }
-        }
-        
-        return ans;
+        return count;
     }
 };
