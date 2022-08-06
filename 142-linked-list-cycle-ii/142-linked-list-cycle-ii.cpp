1b790f7f09812a52b819ee3d4408153c08549e78
VasuDevrani <101383635+VasuDevrani@users.noreply.github.com>
2022-08-06 18:38:19 +0530

    Time: 15 ms (44.57%), Space: 7.6 MB (36.27%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -6,6 +6,23 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// // explanation:
+// the if condition is present at the first encounter of s & f pointers
+// take l1 = distance between head and entry point
+// take l2 = distance from entry point to encounter point
+// take l3 = distance from encounter point to entry point
+
+// as, distance of f = 2* distance of slow
+//     l1+l2+l3+l2 = 2*(l1+l2)
+//     l3 = l1
+    
+// this means that after the first encounter of s & f pointers, distance travelled by any pointer from head to entry point will be equal to l3
+// or, after l3 travel of s and after l1 travel of any pointer from head, they meet at entry
+
+// so simply run them until they become equal, they definitely will
+
+
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
